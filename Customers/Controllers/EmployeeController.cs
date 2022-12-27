d5555d35de6cb5eedc6063bf8364e382cdfa3b8f
Subhasmita Sahu <subhasmita.sahu@gyansys.com>
2022-12-27 15:49:53 +0530

    nan

---
+++ Customers/Controllers/EmployeeController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Customers.Models;
+using System.Net;
+using System.Data;
+namespace Customers.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        private CRUD db = new CRUD();
+
+        public ActionResult Index()
+        {
+            List<Employee> employeeList = new List<Employee>();
+            DataTable dtResult = db.GetAllEmployee();
+            for (int i = 0; i < dtResult.Rows.Count; i++)
+            {
+                Employee employee = new Employee(); //model
+                employee.Id = Convert.ToInt32(dtResult.Rows[i]["Id"]);  
+                employee.FirstName = dtResult.Rows[i]["FirstName"].ToString();
+                employee.LastName = dtResult.Rows[i]["LastName"].ToString();
+                employee.Contact = dtResult.Rows[i]["Contact"].ToString();
+                employee.Email = dtResult.Rows[i]["Email"].ToString();
+                employeeList.Add(employee);
+            }
+            return View(employeeList);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "FirstName,LastName,Contact,Email")] Employee employee)
+        {
+           
+            if (ModelState.IsValid)
+            {
+                int status = db.CreateEmployee(employee.FirstName, employee.LastName, employee.Contact, employee.Email);
+                //int status = db.UpdateEmployee(employee.Id, employee.FirstName, employee.LastName, employee.Contact, employee.Email);
+                ViewBag.StatusMessage = "Employee created successfully";
+            }
+            return RedirectToAction("Index");
+
+        }
+        // GET: Employee/Details/5
+        public ActionResult Details(int id)
+        {
+
+            DataTable dt = db.GetEmployeeById(id);
+            Employee employee = new Employee();
+            employee.Id = Convert.ToInt32(dt.Rows[0]["Id"]);
+            employee.FirstName = Convert.ToString(dt.Rows[0]["FirstName"]);
+            employee.LastName = Convert.ToString(dt.Rows[0]["LastName"]);
+            employee.Contact = Convert.ToString(dt.Rows[0]["Contact"]);
+            employee.Email = Convert.ToString(dt.Rows[0]["Email"]);
+            // employee.Id = Convert.ToInt32(employee.Id);
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            if (employee.Id == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+            
+       
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            DataTable dt = db.GetEmployeeById(id);
+            Employee employee = new Employee();
+            employee.Id = Convert.ToInt32(dt.Rows[0]["Id"]);
+            employee.FirstName = Convert.ToString(dt.Rows[0]["FirstName"]);
+            employee.LastName = Convert.ToString(dt.Rows[0]["LastName"]);
+            employee.Contact = Convert.ToString(dt.Rows[0]["Contact"]);
+            employee.Email = Convert.ToString(dt.Rows[0]["Email"]);
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            } 
+           
+            if (employee.Id == null)
+            { 
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,Contact,Email")] Employee employee)
+        {
+
+            if (ModelState.IsValid)
+            {
+                int status = db.UpdateEmployee(employee);
+               
+                ViewBag.Status = "Updated Employee details successfully";
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            // CRUDModel model = new CRUDModel();
+            db.Delete(id);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Customers/Models/CRUD.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Customers.Models
+{
+    public class CRUD
+    {
+        private string _connectionString = @"Data Source=IN-B2WMGK3\SQLEXPRESS;Initial Catalog=paginationDb;User Id=sa;Password=sa";
+
+        public DataTable GetAllEmployee()
+        {
+            DataTable dataTable = new DataTable();
+            
+            using(SqlConnection sqlConnection = new SqlConnection(_connectionString))
+            {
+                sqlConnection.Open();
+                SqlCommand cmd = new SqlCommand("Select * from Customers", sqlConnection);
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(cmd);
+                dataAdapter.Fill(dataTable);
+                sqlConnection.Close();
+            }
+            return dataTable;
+        }
+        public DataTable GetEmployeeById(int id)
+        {
+            DataTable dataTable = new DataTable();
+
+            // string _connectionString = @"Data Source=IN-B2WMGK3\SQLEXPRESS;Initial Catalog=paginationDb;User Id=sa; Password=sa";
+            
+            using (SqlConnection sqlConnection = new SqlConnection(_connectionString))
+            {
+                sqlConnection.Open();
+
+                SqlCommand cmd = new SqlCommand("Select * from Customers where Id=" + id, sqlConnection);
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+
+                da.Fill(dataTable);
+            }
+
+            return dataTable;
+        }
+
+        public int CreateEmployee(string FirstName, string LastName, string Contact, string Email)
+        {
+            // string strConString = @"Data Source=IN-B2WMGK3\SQLEXPRESS;Initial Catalog=paginationDb;User Id=sa; Password=sa";
+
+            using (SqlConnection con = new SqlConnection(_connectionString))
+            {
+                con.Open();
+                string query = "INSERT INTO Customers(FirstName,LastName,Contact, Email) VALUES (@firstname, @lastname , @contact, @email)";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@firstname", FirstName);
+                cmd.Parameters.AddWithValue("@lastname", LastName);
+                cmd.Parameters.AddWithValue("@contact", Contact);
+                cmd.Parameters.AddWithValue("@email", Email);
+                return cmd.ExecuteNonQuery();
+            }
+        }
+        public int UpdateEmployee(Employee employee)
+        {
+            //string strConString = @"Data Source=IN-B2WMGK3\SQLEXPRESS;Initial Catalog=paginationDb;User Id=sa;Password=sa";
+
+            using (SqlConnection sqlConnection = new SqlConnection(_connectionString))
+            {
+                sqlConnection.Open();
+                string query = "Update Customers SET FirstName=@firstname, LastName=@lastname , Contact=@contact, Email=@email where Id=@id";
+                SqlCommand cmd = new SqlCommand(query, sqlConnection);
+                cmd.Parameters.AddWithValue("@firstname", employee.FirstName);
+                cmd.Parameters.AddWithValue("@lastname", employee.LastName);
+                cmd.Parameters.AddWithValue("@contact", employee.Contact);
+                cmd.Parameters.AddWithValue("@email", employee.Email);
+                cmd.Parameters.AddWithValue("@id", employee.Id);
+                return cmd.ExecuteNonQuery();
+            }
+        }
+
+        public int Delete(int id)
+        {
+            //string strConString = @"Data Source=IN-B2WMGK3\SQLEXPRESS;Initial Catalog=paginationDb;User Id=sa; Password=sa";
+
+            using (SqlConnection sqlConnection = new SqlConnection(_connectionString))
+            {
+                sqlConnection.Open();
+                string query = "Delete from Customers where Id=@id";
+                SqlCommand cmd = new SqlCommand(query, sqlConnection);
+                cmd.Parameters.AddWithValue("@id", id);
+                return cmd.ExecuteNonQuery();
+            }
+        }
+    }
+    
+}
